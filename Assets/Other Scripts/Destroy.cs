8b3c896583a2aef2c1d5eeff598da072e67f4174
JamieNahon <47471301+jamienahon@users.noreply.github.com>
2024-10-08 15:18:34 +1100

    Made boss static until player enters arena

--- Assets/Other Scripts/Destroy.cs
+++ Assets/Other Scripts/Destroy.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     public GameObject barrier;
-    
+    public EnemyStateManager enemyStateManager;
+
+    private void Start()
+    {
+        enemyStateManager.enabled = false;
+    }
+
     public void OnTriggerEnter(Collider Player)
     {
-       // GameObject.Find("Menu").GetComponent<UIController>().OpenTutorial();
+        // GameObject.Find("Menu").GetComponent<UIController>().OpenTutorial();
+        enemyStateManager.enabled = true;
 
-        Destroy(barrier);
+        //Destroy(barrier);
         Destroy(gameObject);
     }
 }
