62f61e8697e2480bc3e214797aa06a3c32cdd80b
IMxSCARED <brianmcnamee7503@gmail.com>
2024-10-21 09:46:27 +1100

    moved outdated animations, added player prefab (merge conflict fix), added in new animations and added new player model (unsused until all animations have been made)

---
+++ Assets/CHARACTER/Player/States/PlayerGetHitState.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGetHitState : PlayerState
+{
+    public override PlayerStateManager stateManager { get; set; }
+
+    public override void EnterState(PlayerStateManager stateManager)
+    {
+        this.stateManager = stateManager;
+        HandleAnimations();
+        SetAnimationParameters();
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+
+    public override void HandleInputs()
+    {
+
+    }
+
+    public override void HandleAnimations()
+    {
+        stateManager.animator.Play("playerHurt");
+    }
+
+    public override void SetAnimationParameters()
+    {
+        stateManager.animator.SetBool("IsMoving", false);
+        stateManager.animator.SetBool("IsSprinting", false);
+        stateManager.animator.SetBool("IsDodging", false);
+        stateManager.animator.SetFloat("HorizontalMovement", 0);
+        stateManager.animator.SetFloat("VerticalMovement", 0);
+    }
+
+    public override void HandleAudio()
+    {
+
+    }
+
+    public override void OnCollisionEnter(Collider collider)
+    {
+
+    }
+
+    public override void OnCollisionExit(Collider collider)
+    {
+
+    }
+}
