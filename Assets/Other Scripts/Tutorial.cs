3f483fc2cb4fe477d50181ac0cbe2f04bba6838c
IMxSCARED <brianmcnamee7503@gmail.com>
2024-10-22 16:46:54 +1100

    Adjusted and added fadein for BossTheme

--- Assets/Other Scripts/Tutorial.cs
+++ Assets/Other Scripts/Tutorial.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEditor;
 using UnityEngine;
 
 
 public class Tutorial : MonoBehaviour
 {
-    public Animation Closetutorial;
+    
     public void OnTriggerEnter(Collider Player)
     {
     GameObject.Find("Menu").GetComponent<UIController>().OpenTutorial();
     }
     public void OnTriggerExit(Collider other)
     {
-      Closetutorial.Play();
+      
       GameObject.Find("Menu").GetComponent<UIController>().CloseTutorial ();
     }
    
