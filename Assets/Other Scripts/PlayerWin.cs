62627cfdc40c80f548714002f619011e8e65856b
JamieNahon <47471301+jamienahon@users.noreply.github.com>
2024-12-03 11:23:43 +1100

    Revert "Added credits"
    This reverts commit 23c6339cf945aa79bc672f3dae2bc53a08a6f637.

--- Assets/Other Scripts/PlayerWin.cs
+++ Assets/Other Scripts/PlayerWin.cs
@@ -5,12 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class PlayerWin : MonoBehaviour
 {
-    public GameObject credits;
     public void OnTriggerEnter(Collider Player)
     {
-        GameObject.Find("Player").GetComponent<PlayerStateManager>().enabled = false;
-        GameObject.Find("Player").GetComponent<PlayerHealthSystem>().enabled = false;
-        GameObject.Find("Menu").GetComponent<UIController>().enabled = false;
-        credits.SetActive(true);
+        SceneManager.LoadScene(0);
     }
 }
